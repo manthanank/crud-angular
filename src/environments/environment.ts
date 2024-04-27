281d7ee54b3ce04248c626711a8ca97eedb62b19
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-04-27 12:21:17 +0530

    Update API URL in environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://light-duck-crown.cyclic.app/api'
+    apiUrl: window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://fair-ruby-newt-gear.cyclic.app/api'
 };
